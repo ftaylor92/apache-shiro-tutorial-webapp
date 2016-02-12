f42dae43fd653b19971fc5e00059454b171cc855
ftaylor92 <ftaylor92@bc.edu>
2016-02-12 13:04:15 -0500

    JDBC without feedback fails

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import org.postgresql.ds.PGSimpleDataSource;
+import org.postgresql.ssl.NonValidatingFactory;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		PGSimpleDataSource f= null;
+		
+		NonValidatingFactory g= null;
+		
+		//config.setDriverProperties(driverProperties);
+		
+	}
+
+}
